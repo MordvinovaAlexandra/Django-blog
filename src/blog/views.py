ddf8baa49078a501cd91bfb8435448789090f4db
Alexandra Mordvinova <mordvinova.alexandra@gmail.com>
2024-02-05 22:25:31 +0300

    post list view & template added

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post/list.html', {"posts": posts})
+
+
 # Create your views here.
